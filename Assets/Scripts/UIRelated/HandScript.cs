c93227755c9780c58070b5976771f5f2567e4695
1997Gongquan <2673077461@qq.com>
2020-10-16 23:03:29 +0800

    2020-10-16 技能拖拽

---
+++ Assets/Scripts/UIRelated/HandScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandScript : MonoBehaviour
+{
+    /// <summary>
+    /// 单例对象
+    /// </summary>
+    private static HandScript instance;
+
+    public static HandScript MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<HandScript>();
+            }
+
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// 当前鼠标拉取的对象
+    /// </summary>
+    public IMoveable MyMoveable { get; set; }
+
+    /// <summary>
+    /// 鼠标拉取对象的图标
+    /// </summary>
+    private Image icon;
+
+    /// <summary>
+    /// 拉取物体距离鼠标的偏移量
+    /// </summary>
+    [SerializeField]
+    private Vector3 offset;
+
+    void Start ()
+    {
+        icon = GetComponent<Image>();	
+    }
+	
+    void Update ()
+    {
+        // 使拉取的物体跟随鼠标
+        icon.transform.position = Input.mousePosition+offset;
+    }
+
+    /// <summary>
+    /// 将拉取物体复制到鼠标位置，跟随鼠标移动
+    /// </summary>
+    /// <param name="moveable">可移动的对象</param>
+    public void TakeMoveable(IMoveable moveable)
+    {
+        this.MyMoveable = moveable;
+        icon.sprite = moveable.MyIcon;
+        icon.color = Color.white;
+    }
+
+    public IMoveable Put()
+    {
+        IMoveable tmp = MyMoveable;
+        MyMoveable = null;
+        icon.color = new Color(0, 0, 0, 0);
+        return tmp;
+    }
+}
---
+++ Assets/Scripts/UIRelated/Imoveable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public interface IMoveable
+{
+    Sprite MyIcon
+    {
+        get;
+    }
+}
