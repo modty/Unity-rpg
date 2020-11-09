6c573ccf85b188b201b3f3d5a25a174bc5f0f3ab
1997Gongquan <2673077461@qq.com>
2020-11-09 21:50:42 +0800

    2020-11-09 角色血条、蓝条和属性面板

---
+++ Assets/Scripts/UI/BarScript.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+ /// <summary>
+    /// 当前绑定对象图片
+    /// </summary>
+    [SerializeField]
+    private Image content;
+
+    /// <summary>
+    /// UI条上的文本
+    /// </summary>
+    [SerializeField]
+    private Text statValue;
+
+    /// <summary>
+    /// 当前填充值
+    /// </summary>
+    private float currentFill;
+
+    private float overflow;
+
+    /// <summary>
+    /// 平滑速度
+    /// </summary>
+    [SerializeField]
+    private float lerpSpeed;
+
+    /// <summary>
+    /// 最大当前当前生命值、魔法值
+    /// </summary>
+    private float maxValue;
+
+    public float MaxValue
+    {
+        get => maxValue;
+        set => maxValue = value;
+    }
+
+    /// <summary>
+    /// 当前当前生命值、魔法值
+    /// </summary>
+    private float currentValue;
+
+    public bool IsFull
+    {
+        get { return content.fillAmount == 1; }
+    }
+
+    public float MyOverflow
+    {
+        get
+        {
+            float tmp = overflow;
+            overflow = 0;
+            return tmp;
+        }
+    }
+
+    /// <summary>
+    /// 正确更新CurrentValue值
+    /// </summary>
+    public float CurrentValue
+    {
+        get
+        {
+
+            return currentValue;
+        }
+
+        set
+        {
+            if (value > MaxValue)// 确保数值不上溢
+            {
+                overflow = value - MaxValue;
+                currentValue = MaxValue;
+            }
+            else if (value < 0) // 确保数值不下溢
+            {
+                currentValue = 0;
+            }
+            else //数值在0~maxValue之间
+            {
+                currentValue = value;
+            }
+
+            // 计算UI填充长度
+            currentFill = currentValue / MaxValue;
+
+            if (statValue != null)
+            {
+                // 设置文本
+                statValue.text = currentValue + " / " + MaxValue;
+            }
+
+        }
+    }
+    void Update()
+    {
+        HandleBar();
+    }
+    /// <summary>
+    /// 初始化数据
+    /// </summary>
+    /// <param name="currentValue">当前UI条代表的值</param>
+    /// <param name="maxValue">UI代表的最大值</param>
+    public void Initialize(float currentValue, float maxValue)
+    {
+        if (content == null)
+        {
+            content = GetComponent<Image>();
+        }
+
+        MaxValue = maxValue;
+        CurrentValue = currentValue;
+        content.fillAmount = CurrentValue / MaxValue;
+    }
+
+    /// <summary>
+    /// 更新UI条
+    /// </summary>
+    private void HandleBar()
+    {
+        if (currentFill != content.fillAmount) // 在数据变化后才更新UI
+        {
+            content.fillAmount = Mathf.MoveTowards(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+
+    }
+
+    public void Reset()
+    {
+        content.fillAmount = 0;
+    }
+}
---
+++ Assets/Scripts/UI/StatScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatScript : MonoBehaviour
+{
+    private CharacterState controlledCharacterState;
+
+    public CharacterState ControlledCharacterState
+    {
+        get => controlledCharacterState;
+        set => controlledCharacterState = value;
+    }
+
+    private static StatScript instance;
+    public static StatScript Instance => instance;
+    [SerializeField]
+    private Text[] attributeNums;
+    [SerializeField]
+    private BarScript healthBar;
+    [SerializeField]
+    private BarScript manaBar;
+    
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Initial()
+    {
+        healthBar.Initialize(controlledCharacterState.Health[0],controlledCharacterState.Health[1]);
+        manaBar.Initialize(controlledCharacterState.Mana[0],controlledCharacterState.Mana[1]);
+        for (int i = 0; i < attributeNums.Length; i++)
+        {
+            if (i==4)
+            {
+                attributeNums[i].text = ((float)controlledCharacterState.BaseAttribute[i]/100).ToString("0.00");
+            }
+            else
+            {
+                attributeNums[i].text = controlledCharacterState.BaseAttribute[i].ToString();
+            }
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            healthBar.CurrentValue -= 100;
+            manaBar.CurrentValue -= 100;
+        }
+    }
+}
