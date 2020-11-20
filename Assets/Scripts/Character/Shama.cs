f0f563aeee108dbaee06cbad99e46e40ba288b23
1997Gongquan <2673077461@qq.com>
2020-11-20 23:34:36 +0800

    2020-11-20 萨满技能释放

--- Assets/Scripts/Character/Shama.cs
+++ Assets/Scripts/Character/Shama.cs
@@ -1,21 +1,72 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Shama:MonoBehaviour
 {
     [SerializeField]
     private Animator selfAnimator;
-    
     private static readonly int X = Animator.StringToHash("X");
     private static readonly int Move = Animator.StringToHash("Move");
     private static readonly int Spell1 = Animator.StringToHash("Spell1");
     private static readonly int Spell2 = Animator.StringToHash("Spell2");
+    [SerializeField]
+    private GameObject dollPrefab;
+    /// <summary>
+    /// 人偶(最大为5个)
+    /// </summary>
+    private List<ShamaDoll> dolls;
+    private float endDistance=1.8f;
+    private float startDistance = 0.8f;
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void Awake()
     {
-        if (other.collider.tag.Equals("Player"))
+    }
+
+    private void Start()
+    {
+        dolls=new List<ShamaDoll>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
         {
             selfAnimator.SetBool(Spell1,true);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            selfAnimator.SetBool(Spell1,true);
+            
+            
+        }
+    }
+
+    /// <summary>
+    /// 创造木偶
+    /// </summary>
+    /// <param name="num"></param>
+    /// <param name="angle"></param>
+    public void CreateDoll(int angle)
+    {
+        if (dolls.Count <= 5)
+        {
+            ShamaDoll shamaDoll = Instantiate(dollPrefab).GetComponent<ShamaDoll>();
+            dolls.Add(shamaDoll);
+            var position = transform.position;
+            Vector2 target=new Vector2((float) (endDistance*Math.Cos(angle)+position.x),(float) (endDistance*Math.Sin(angle)+position.y));
+            Vector2 start=new Vector2((float) (startDistance*Math.Cos(angle)+position.x),(float) (startDistance*Math.Sin(angle)+position.y));
+            shamaDoll.Position(start);
+            shamaDoll.SpriteRenderer.enabled = true;
+            shamaDoll.MoveTowards(target);
+        }
+        else
+        {
+            
         }
     }
 }
---
+++ Assets/Scripts/Character/ShamaDoll.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 萨满人偶
+/// </summary>
+public class ShamaDoll : MonoBehaviour
+{
+    private static ShamaDoll instance;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private bool isMove;
+    private float moveSpeed;
+    private Vector2 targetPosition;
+    private SpriteRenderer sprite;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb=GetComponent<Rigidbody2D>();
+        moveSpeed = 0.01f;
+        sprite = GetComponent<SpriteRenderer>();
+        instance = this;
+    }
+
+    public static ShamaDoll Instance => instance;
+    public Animator Animator => animator;
+
+    private void Update()
+    {
+        if (isMove)
+        {
+            Move();
+        }
+    }
+
+    public void Position(Vector2 vector2)
+    {
+        rb.position = vector2;
+    }
+
+    public void MoveTowards(Vector2 vector2)
+    {
+        if (!isMove)
+        {
+            targetPosition = vector2;
+            isMove = true;
+        }
+    }
+
+    private void Move()
+    {
+        Position(Vector2.MoveTowards(rb.position,targetPosition,moveSpeed));
+    }
+
+    public SpriteRenderer SpriteRenderer => sprite;
+}
